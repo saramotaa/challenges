955c320ec447da56c70e5554815ad1337f99cb0e
Sara Mota <saraamotalima@gmail.com>
2022-12-29 15:10:02 -0300

    soma simples

---
+++ desafio-soma-simples.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <title>DESAFIO: Soma Simples</title>
+</head>
+
+<body>
+<pre>
+<?php
+
+// DESAFIO:
+// Leia dois valores inteiros identificados como variáveis A e B. Calcule a soma entre elas e chame essa variável de SOMA. A seguir escreva o valor desta variável.
+// Use a função "echo" formatar e imprimir sua(s) Saída(s).
+
+// ENTRADA:
+// O arquivo de entrada contém 2 valores inteiros.
+
+// SAÍDA:
+// Imprima a variável SOMA com todas as letras maiúsculas, inserindo um espaço em branco antes e depois do símbolo de igualdade, seguido pelo valor correspondente à soma de A e B como no exemplo:
+// SOMA = 80
+
+$a = "30";
+$b = "10";
+$soma = ($a + $b);
+echo "SOMA = " . ($soma);
+
+?>
+</pre>
+</body>
+
+</html>
